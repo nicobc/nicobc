4289eeb8681b1cb0753ffc65896ab9b0395b7d04
Nicolas Contreras <nicolas.b.contreras@gmail.com>
2026-05-22 01:44:00 +0200

    style(ux): drop About page, simplify navbar to NC monogram [EPIC-006/T3]

--- app/src/app/navbar/navbar.ts
+++ app/src/app/navbar/navbar.ts
@@ -1,31 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
-import { Dropdown, DropdownItem } from '../shared/dropdown/dropdown';
+import { RouterLink } from '@angular/router';
 import { ThemeToggle } from '../theme-toggle/theme-toggle';
-
-interface NavLink {
-  label: string;
-  route: string;
-}
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive, FaIconComponent, Dropdown, ThemeToggle],
+  imports: [RouterLink, ThemeToggle],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
-  readonly bars = faBars;
-  readonly xmark = faXmark;
-  isOpen = false;
-
-  readonly navLinks: NavLink[] = [{ label: 'About', route: '/about' }];
-
-  readonly mobileItems: DropdownItem[] = [{ label: 'Home', route: '/home' }, ...this.navLinks];
-
-  toggleMenu(): void {
-    this.isOpen = !this.isOpen;
-  }
+  readonly brandPrefix = 'N';
+  readonly brandSuffix = 'C';
 }
